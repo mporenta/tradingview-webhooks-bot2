bondjames12/tradingview-webhooks-bot

Add interfaces for Tbot on Tradingboat
Signed-off-by: Plusgenie Ltd <aladdin@plusgenie.com>

src/settings.py
@@ -1,9 +1,9 @@
 # actions
-REGISTERED_ACTIONS = ['PrintData']
+REGISTERED_ACTIONS = ['RedisPubAction']
 
 # events
 REGISTERED_EVENTS = ['WebhookReceived']
 
 # links
-REGISTERED_LINKS = [('PrintData', 'WebhookReceived')]
+REGISTERED_LINKS = [('RedisPubAction', 'WebhookReceived')]
 

mporenta/tradingview-webhooks-bot2

src/settings.py
@@ -1,9 +1,9 @@
 # actions
-REGISTERED_ACTIONS = ['PrintData']
+REGISTERED_ACTIONS = ['RedisPubActionClients']
 
 # events
 REGISTERED_EVENTS = ['WebhookReceived']
 
 # links
-REGISTERED_LINKS = [('PrintData', 'WebhookReceived')]
+REGISTERED_LINKS = [('RedisPubActionClients', 'WebhookReceived')]
 